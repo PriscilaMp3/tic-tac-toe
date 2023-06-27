2a6c6326070218dad848bdb768bddc4bfcce1c5c
PriscilaMp3 <135663453+PriscilaMp3@users.noreply.github.com>
2023-06-27 16:10:30 -0600

    Update scritp.js
    Se creo la funcionalidad del juego. Js

--- src/scritp.js
+++ src/scritp.js
@@ -1,20 +1,24 @@
-
-// Variables globales
-let board = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
-const winningCombinations = [
-  [0, 1, 2], [3, 4, 5], [6, 7, 8], // Filas
-  [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columnas
-  [0, 4, 8], [2, 4, 6] // Diagonales
+//ARREGLAR QUE CUANDO TOCA PARA ESCRIBIR QUE NO PUEDA ESCRIBIR HASTA QUE LA MAQUINA NO HAYA MARCADO
+let casilla = ["", "", "", "", "", "", "", "", ""]; //casillas//
+let JugadorActual = "X";
+const combinacionesGanadoras = [
+  [0, 1, 2], // Filas
+  [3, 4, 5], // Filas
+  [6, 7, 8], // Filas
+  [0, 3, 6], // Columnas
+  [1, 4, 7], // Columnas
+  [2, 5, 8], // Columnas
+  [0, 4, 8], // Diagonales
+  [2, 4, 6], // Diagonales
 ];
 
-// Función para verificar si alguien ha ganado
+// Determinar con una funcion y un for el ganador //
 function checkWinner(player) {
-  for (let combination of winningCombinations) {
+  for (let combinacion of combinacionesGanadoras) {
     if (
-      board[combination[0]] === player &&
-      board[combination[1]] === player &&
-      board[combination[2]] === player
+      casilla[combinacion[0]] === player &&
+      casilla[combinacion[1]] === player &&
+      casilla[combinacion[2]] === player
     ) {
       return true;
     }
@@ -22,33 +26,34 @@ function checkWinner(player) {
   return false;
 }
 
-// Función para verificar si hay un empate
+// Determinar con una funcion si hay un empate
 function checkDraw() {
-  return !board.includes('');
+  return !casilla.includes("");
 }
 
 // Función para hacer un movimiento del jugador
 function playerMove(index) {
-  if (board[index] === '') {
+  if (casilla[index] === "") {
     // Realizar el movimiento del jugador
-    board[index] = currentPlayer;
-    document.getElementsByClassName('button-option')[index].innerText = currentPlayer;
+    casilla[index] = JugadorActual;
+
+    document.getElementsByClassName("celda")[index].innerText = JugadorActual;
 
     // Verificar el resultado
-    if (checkWinner(currentPlayer)) {
+    if (checkWinner(JugadorActual)) {
       setTimeout(() => {
-        showMessage('¡Has ganado! Inténtalo de nuevo.');
+        showMessage("¡Has ganado!");
         showPopup();
       }, 100);
     } else if (checkDraw()) {
       setTimeout(() => {
-        showMessage('¡Empate! Inténtalo de nuevo.');
+        showMessage("¡Empate!");
         showPopup();
       }, 100);
     }
 
     // Cambiar el jugador actual
-    currentPlayer = 'O';
+    JugadorActual = "O";
 
     // Dejar que el ordenador haga su movimiento
     setTimeout(computerMove, 500);
@@ -58,8 +63,8 @@ function playerMove(index) {
 // Función para que el ordenador haga su movimiento
 function computerMove() {
   // Obtener una lista de movimientos válidos
-  const validMoves = board.reduce((acc, cell, index) => {
-    if (cell === '') {
+  const validMoves = casilla.reduce((acc, cell, index) => {
+    if (cell === "") {
       acc.push(index);
     }
     return acc;
@@ -68,71 +73,76 @@ function computerMove() {
   // Elegir un movimiento al azar de los disponibles
   const randomIndex = Math.floor(Math.random() * validMoves.length);
   const computerChoice = validMoves[randomIndex];
+  casilla[computerChoice - 1];
 
   // Realizar el movimiento del ordenador
-  board[computerChoice] = 'O';
-  document.getElementsByClassName('button-option')[computerChoice].innerText = 'O';
+  casilla[computerChoice] = "O";
+  document.getElementsByClassName("celda")[computerChoice].innerText = "O";
 
   // Verificar el resultado
-  if (checkWinner('O')) {
+  if (checkWinner("O")) {
     setTimeout(() => {
-      showMessage('¡Has perdido! Inténtalo de nuevo.');
+      showMessage("¡Has perdido!");
       showPopup();
     }, 100);
   } else if (checkDraw()) {
     setTimeout(() => {
-      showMessage('¡Empate! Inténtalo de nuevo.');
+      showMessage("¡Empate!");
       showPopup();
     }, 100);
   }
 
   // Cambiar el jugador actual
-  currentPlayer = 'X';
+  JugadorActual = "X";
 }
 
 // Función para reiniciar el juego
 function resetGame() {
-  board = ['', '', '', '', '', '', '', '', ''];
-  currentPlayer = 'X';
-  const buttons = document.getElementsByClassName('button-option');
+  casilla = ["", "", "", "", "", "", "", "", ""];
+  JugadorActual = "X";
+  const buttons = document.getElementsByClassName("celda");
   for (let button of buttons) {
-    button.innerText = '';
+    button.innerText = "";
   }
   hidePopup();
 }
 
 // Funciones de la ventana emergente
 function showPopup() {
-  const popup = document.querySelector('.popup');
-  popup.classList.remove('hide');
+  const popup = document.querySelector(".popup");
+  popup.classList.remove("hide");
 }
 
 function hidePopup() {
-  const popup = document.querySelector('.popup');
-  popup.classList.add('hide');
+  const popup = document.querySelector(".popup");
+  popup.classList.add("hide");
 }
 
+//realizar un modal como mensaje de entrada al juego QUE DIGA INICIAR EL JUEGO......
+
 function showMessage(message) {
-  const messageElement = document.getElementById('message');
+  const messageElement = document.getElementById("message");
   messageElement.innerText = message;
 }
 
 // Reiniciar juego al hacer clic en "Restart"
-document.getElementById('restart').addEventListener('click', resetGame);
+
+// reiniciar el juego por medio de una actualizacion de la pagina.....
+document.getElementById("restart").addEventListener("click", resetGame);
 
 // Iniciar un nuevo juego al hacer clic en "New Game" en la ventana emergente
-document.getElementById('new-game').addEventListener('click', () => {
+document.getElementById("new-game").addEventListener("click", () => {
   hidePopup();
   resetGame();
 });
 
-
 // Obtén una referencia a cada botón del juego
-const buttons = document.getElementsByClassName('button-option');
+const buttons = document.getElementsByClassName("celda");
 
 // Asigna el evento de clic a cada botón
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function() {
+  buttons[i].addEventListener("click", function () {
     playerMove(i);
   });
 }
+
